40d6a5bb908f39bf2b28e854494bcfb84438b8ed
Vashishta Parikipandla <vashishtaparikipandla@Vashishtas-MacBook-Air.local>
2026-09-20 19:12:23 +0530

    feat: Restore Astryx design system and wrap app in mobile AppShell frame

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,1 @@
-export default { typescript: { ignoreBuildErrors: true }, eslint: { ignoreDuringBuilds: true } }
+export default { typescript: { ignoreBuildErrors: true }, allowedDevOrigins: ['411e9a1f921783c3-49-207-207-47.serveousercontent.com', 'https://411e9a1f921783c3-49-207-207-47.serveousercontent.com'] }
